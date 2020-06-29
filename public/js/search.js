f9df9a8c2591895714fd8f5a149a6a829ae0370e
Sahran Khuwaja <sahrankhuwaja733@gmail.com>
2020-06-30 01:08:15 +0500

    Final Touch

--- public/js/search.js
+++ public/js/search.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
 
 
     check();
+    getRecommendations();
 })
 
 
@@ -35,6 +36,31 @@ function check(callback){
         perform(data);
     });
     
+}
+
+const getRecommendations = async () => {
+
+    $.get('/Profile/Recommendations/Get?k=3', undefined, (data, status, xhr) => {
+        if (status === 'success') {
+            getViews();
+            if (data.Neighbours.length !== 0) {
+                getUserInfo(data)
+            } 
+        }
+    })
+}
+
+
+const getUserInfo = async (data) => {
+
+    $.get('/Profile/Recommendations/User/Data/Fetch', data, (data, status, xhr) => {
+        if (status === 'success') {
+            renderNeighbours(data.neighbours);
+            renderPredictions(data.predictions);
+        }
+
+
+    })
 }
 
 
@@ -74,4 +100,50 @@ function perform(data){
   document.querySelector('#result-info-head').innerHTML = dataHtml2;
 
 
+}
+
+const getViews = async()=>{
+
+    $.get('/Profile/Recommendations/Get/Views',undefined,(data,status,xhr)=>{
+            if(status==='success'){
+               if(data.length!==0){
+                   getDataOfHighlyViewedProfiles(data)
+               }
+            }        
+    })
+
+}
+
+const getDataOfHighlyViewedProfiles = async(profileData)=>{
+ 
+    $.get('/Profile/Recommendations/HighViewed/Data/Fetch',{profileData},(data,status,xhr)=>{
+        if(status==='success'){
+            renderHighViewedProfiles(data)
+        }
+    })
+
+}
+
+const renderNeighbours = async(data)=>{
+
+    const parent = document.querySelector('#sticky-sidebar');
+    const template = document.querySelector('#side-bar-template').innerHTML;
+    const html = Mustache.render(template,{Title:'Nearest Neighbours',data, recommend:true});
+    parent.insertAdjacentHTML('beforeend',html);
+}
+
+const renderPredictions = async(data)=>{
+
+    const parent = document.querySelector('#sticky-sidebar');
+    const template = document.querySelector('#side-bar-template').innerHTML;
+    const html = Mustache.render(template,{Title:'Predictions',data,recommend:true,predict:true});
+    parent.insertAdjacentHTML('beforeend',html);
+}
+
+const renderHighViewedProfiles = async(data)=>{
+
+    const parent = document.querySelector('#sticky-sidebar');
+    const template = document.querySelector('#side-bar-template').innerHTML;
+    const html = Mustache.render(template,{Title:'Most Viewed Profiles',data});
+    parent.insertAdjacentHTML('beforeend',html);
 }
